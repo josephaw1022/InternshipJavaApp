ee160fc6784c0aa754ef89255aeec3dd5c2b2a58
jacktrig <jacktrig@email.sc.edu>
2021-10-14 21:54:52 -0400

    added User class and method stubs

---
+++ User.java
@@ -0,0 +1,21 @@
+public abstract class User {
+  protected String id;
+  protected String firstName;
+  protected String lastName;
+  protected String username;
+  protected String password;
+  protected String email;
+  protected int phone;
+  
+  public User(String id, String firstName, String lastName, String username, String password, String email, int phone) {
+
+  }
+  public boolean checkUnique(String username, String password, String email, int phone) {
+    //TODO
+    return true;
+  }
+  protected boolean changePassword() {
+    //TODO
+    return true;
+  }
+}
