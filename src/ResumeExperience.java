116dbd0391fe5c9056254e093b32bb41200649ec
jacktrig <jacktrig@email.sc.edu>
2021-10-17 23:40:29 -0400

    added resumes json, job listing class, review, resumeExperience

---
+++ src/ResumeExperience.java
@@ -0,0 +1,23 @@
+package src;
+
+import java.util.ArrayList;
+
+ public class ResumeExperience {
+     private String companyName;
+     private String experienceLocation;
+     private String positionTitle;
+     private String datesOfExperience;
+     private ArrayList<String> experienceDetails;
+
+     public ResumeExperience(String experienceTitle, String experienceLocation, String positionTitle, String datesOfExperience){
+
+     }
+
+     public ArrayList<String> addExperienceDetail(String detailDescription){
+
+     }
+
+     public boolean removeExperienceDetail(String detailDescription){
+
+     }
+ }
