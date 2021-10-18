d51d946acedb02ec7684b7d16961509347d49944
Zandair Watkins <ethanwatkins1720@gmail.com>
2021-10-17 21:55:22 -0400

    Added Employee class

---
+++ Employee.java
@@ -0,0 +1,12 @@
+public class Employee {
+    private String companyName;
+    private String jobTitle;
+
+    public Employee(String companyName, String jobTitle){
+
+    }
+
+    public Review addStudentReview(){
+
+    }
+}
