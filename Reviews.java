c3dc2a189f65c5d738589a68f54c667cb43cc6b9
Zandair Watkins <ethanwatkins1720@gmail.com>
2021-10-17 21:39:14 -0400

    Added Reviews class

---
+++ Reviews.java
@@ -0,0 +1,23 @@
+public class Reviews {
+    private User writer;
+    private String comment;
+    private double rating;
+
+    public Reviews(User writer, String comment, double score){
+
+    }
+
+    public User getWriter(){
+
+    }
+
+    public String getComment(){
+
+    }
+
+    public double getRating(){
+
+    }
+
+
+}
