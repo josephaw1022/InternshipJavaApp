52f21ab53cde31045efa4c1e9365a1c76a069bae
josephaw1022 <josephsims1@gmail.com>
2021-10-18 08:40:16 -0400

    scaffold company endpoint

---
+++ src/EndpointCompany.java
@@ -0,0 +1,36 @@
+
+import java.util.ArrayList; 
+
+
+
+public class EndpointCompany {
+        private ArrayList<Company> list ; 
+
+    public void create(){
+
+    }
+
+    
+    /** 
+     * @return User
+     */
+    public Company readDetail(){ 
+
+    }
+
+    
+    /** 
+     * @return User[]
+     */
+    public Company[] readList() {
+        
+    }
+
+    public void update(){ 
+
+    }
+    
+    public void delete(){
+
+    }
+}
