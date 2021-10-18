7f2ef2baff4c7faed249fdfbd22425c4a5b34f70
Brian Bongermino <firenight485@gmail.com>
2021-10-18 07:46:00 -0400

    repushing employee

---
+++ src/Employee.java
@@ -0,0 +1,14 @@
+package src;
+public class Employee{
+    private String companyName;
+    private String jobTitle;
+
+    public Employee(String companyName, String jobTitle){
+
+    }
+
+    public Review addStudentReview(){
+
+    }
+    
+}
