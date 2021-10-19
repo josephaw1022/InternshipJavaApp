36f36695c7601be028e08058883cc2d861a84412
jacktrig <jacktrig@email.sc.edu>
2021-10-19 09:59:09 -0400

    added UUID printer class, added resume json into student json

--- src/ApplicationUI.java
+++ src/ApplicationUI.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.UUID;
 
 public class ApplicationUI {
     private Scanner scanner;
---
+++ src/UUIDPrint.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class UUIDPrint {
+  public static void main(String args[]) {
+    final String uuid = UUID.randomUUID().toString();
+    System.out.println(uuid);
+  }
+}
