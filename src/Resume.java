79ace6b6fb05da6b00f8d1e55add17f5015ad361
Brian Bongermino <firenight485@gmail.com>
2021-10-19 09:41:29 -0400

    adds some javadoc

--- src/Resume.java
+++ src/Resume.java
@@ -6,24 +6,38 @@ public class Resume {
   private ArrayList<ResumeExperience> workExp;
   private ArrayList<String> skills;
   private ArrayList<String> currentClasses;
-
+  /**
+   * calls and assign the  strings and the array lists 
+   */
   public Resume() {
     this.workExp = new ArrayList<ResumeExperience>();
     this.skills = new ArrayList<String>();
     this.currentClasses = new ArrayList<String>();
   }
-  
+  /**
+   * creates the resume constructor 
+   * @param id
+   * @param workExp
+   * @param skills
+   * @param currentClasses
+   */
   public Resume(String id, ArrayList<ResumeExperience> workExp, ArrayList<String> skills, ArrayList<String> currentClasses) {
     this.id = id;
     this.workExp = workExp;
     this.skills = skills;
     this.currentClasses = currentClasses;
   }
-  
+  /**
+   * adds skill to the arraylist 
+   * @param skill
+   */
   public void addSkill(String skill) {
     skills.add(skill);
   }
-
+  /**
+   * adds class to the array list
+   * @param className
+   */
   public void addClass(String className) {
     currentClasses.add(className);
   }
