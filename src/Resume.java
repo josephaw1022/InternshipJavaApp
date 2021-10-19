9b5dec756ebb27a5116c119df58102d56c57d1e5
Brian Bongermino <firenight485@gmail.com>
2021-10-19 09:21:22 -0400

    pushing updated classes

--- src/Resume.java
+++ src/Resume.java
@@ -7,18 +7,25 @@ public class Resume {
   private ArrayList<String> skills;
   private ArrayList<String> currentClasses;
 
-  public Resume() {}
+  public Resume() {
+    this.workExp = new ArrayList<ResumeExperience>();
+    this.skills = new ArrayList<String>();
+    this.currentClasses = new ArrayList<String>();
+  }
   
   public Resume(String id, ArrayList<ResumeExperience> workExp, ArrayList<String> skills, ArrayList<String> currentClasses) {
-
+    this.id = id;
+    this.workExp = workExp;
+    this.skills = skills;
+    this.currentClasses = currentClasses;
   }
   
   public void addSkill(String skill) {
-
+    skills.add(skill);
   }
 
   public void addClass(String className) {
-
+    currentClasses.add(className);
   }
 	
 }
