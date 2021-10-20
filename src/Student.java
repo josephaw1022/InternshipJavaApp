4fd9f220a2482d18505ebda0597ede0b806ebd8e
unknown <jdhennings@yahoo.com>
2021-10-19 20:19:21 -0400

    Implemented changePassword method. Updated id to be UUID. Also contains updated Student

--- src/Student.java
+++ src/Student.java
@@ -3,33 +3,49 @@
  */
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class Student extends User {
     private String major;
     private double GPA;
     private ArrayList<Resume> resumes;
+    private ArrayList<Review> reviews;
     
     public Student() {
-        
+      
     }
-    public Student(String major, String studentID) {
-        this.major = major;
-        resumes = new ArrayList<Resume>();
+
+    public Student(UUID id,
+                   String firstName,
+                   String lastName,
+                   String username,
+                   String password,
+                   String email,
+                   int phone,
+                   String major,
+                   String studentID) {
+      super(id, firstName, lastName, username, password, email, phone);
+      this.major = major;
+      resumes = new ArrayList<Resume>();
     }
 
     public String getMajor() {
-      return "str";
+      return major;
     }
 
     public void addResume(Resume resume) {
-
+      resumes.add(resume);
     }
 
-    public Resume createResume() {
-      return new Resume();
+    public Resume createResume(String id, ArrayList<ResumeExperience> workExp, ArrayList<String> skills, ArrayList<String> currentClasses) {
+      Resume newResume = new Resume(id, workExp, skills, currentClasses);
+      resumes.add(newResume);
+      return newResume;
     }
 
-    public Review addReview() {
-      return new Review();  
+    public Review addReview(User writer, String comment, double score) {
+      Review newReview = new Review(writer, comment, score);
+      reviews.add(newReview);
+      return newReview;  
     }
 }
