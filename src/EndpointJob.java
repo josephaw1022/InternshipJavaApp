8419e4222bf6bbc32b8315d31da52bd76f0b64e9
jacktrig <jacktrig@email.sc.edu>
2021-10-21 09:50:23 -0400

    copying over cconflicts with my main

--- src/EndpointJob.java
+++ src/EndpointJob.java
@@ -1,47 +1,50 @@
 import java.util.ArrayList;
-import java.util.*;
 
 public class EndpointJob {
 
   private ArrayList<JobListing> list;
-  
-  public void create(JobListing job){
-      this.list.add(job);
+
+  public static void create(JobListing job){
+      this.list.add(job)
     }
 
   /**
    * @return Job
    */
-  public JobListing readDetail(UUID id) {
+  public static JobListing readDetail(int id) {
+
     for (int i = 0; i < this.list.size(); i++) {
-      if(this.list.get(i).id == id) {
-        return this.list.get(i);
+      if (this.list.get(i).id == id) {
+        return this.list.get(i)
       }
     }
-    return new JobListing();
   }
 
   /** 
      * @return Job[]
      */
-    public ArrayList<JobListing> readList() {
-      return this.list;
+    public static JobListing[] readList() {
+      return this.list 
     }
 
-  public void update(JobListing job){ 
+  public static boolean update(JobListing job){ 
       for (int i = 0; i < this.list.size(); i++) {
         if (this.list.get(i).id == job.id) {
-          job = this.list.get(i);
+          this.list.set(i,job)
+          return true 
         }
       } 
+      return false 
     }
 
-  public void delete(UUID id){
-      for (int i = 0; i < this.list.size(); i++) {
-        if (this.list.get(i).id == id) {
-          this.list.remove(i);
-        }
+  public static boolean delete(int id) {
+    for (int i = 0; i < this.list.size(); i++) {
+      if (this.list.get(i).id == id) {
+        this.list.remove(i);
+        return true 
       }
     }
+    return false 
+  }
 
 }
