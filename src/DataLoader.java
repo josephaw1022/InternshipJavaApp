7b6f659ccc6409264f97aa7ae52ef683820cc155
jacktrig <jacktrig@email.sc.edu>
2021-10-21 09:46:58 -0400

    small edit to data constants and loader

--- src/DataLoader.java
+++ src/DataLoader.java
@@ -13,18 +13,18 @@ public class DataLoader extends DataConstants {
     try {
       FileReader reader = new FileReader(USER_FILE_NAME);
       JSONParser parser = new JSONParser();	
-      JSONArray peopleJSON = (JSONArray)new JSONParser().parse(reader);
+      JSONArray studentsJSON = (JSONArray)new JSONParser().parse(reader);
 	
-      for(int i=0; i < peopleJSON.size(); i++) {
-        JSONObject personJSON = (JSONObject)peopleJSON.get(i);
-        UUID id = UUID.fromString((String)personJSON.get(USER_ID));
-	String userName = (String)personJSON.get(USER_USER_NAME);
-	String password = (String)personJSON.get(USER_PASSWORD);
-	String firstName = (String)personJSON.get(USER_FIRST_NAME);
-	String lastName = (String)personJSON.get(USER_LAST_NAME);
-	String email = (String)personJSON.get(USER_EMAIL);
-	int phoneNumber = ((Long)personJSON.get(USER_PHONE_NUMBER)).intValue();	
-	String major = (String)personJSON.get(USER_MAJOR);
+      for(int i=0; i < studentsJSON.size(); i++) {
+        JSONObject studentJSON = (JSONObject)studentsJSON.get(i);
+        UUID id = UUID.fromString((String)studentJSON.get(USER_ID));
+	String userName = (String)studentJSON.get(USER_USER_NAME);
+	String password = (String)studentJSON.get(USER_PASSWORD);
+	String firstName = (String)studentJSON.get(USER_FIRST_NAME);
+	String lastName = (String)studentJSON.get(USER_LAST_NAME);
+	String email = (String)studentJSON.get(USER_EMAIL);
+	int phoneNumber = ((Long)studentJSON.get(USER_PHONE_NUMBER)).intValue();	
+	String major = (String)studentJSON.get(USER_MAJOR);
 	students.add(new Student(id, firstName, lastName, userName, password, email, phoneNumber, major));
 	}
 	
